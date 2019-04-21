d5af163bba1dbb724140bc80e4bd639b9705b326
Nathan Bliss <ndb@software-automation.com>
2019-04-21 12:39:15 +0100

    Improvements

--- pretty_print.go
+++ pretty_print.go
@@ -1,102 +1,141 @@
 package main
-
+ 
 import (
-	"encoding/json"
-	"io/ioutil"
-  "fmt"
-  "os"
+    //"fmt"
+    "encoding/json"
+    "os"
+    //"io"
+    "io/ioutil"
+    "reflect"
+    "strconv"
 )
-/////////////////////////////////////////////////////////////////////////////////
-func get_space_string(indentation_level int) string {
-  space_string := ""
-  
-  for count := 0; count < indentation_level; count++ {
-    space_string += " "
-  }
-  
-  return space_string
-}
-/////////////////////////////////////////////////////////////////////////////////
-func recurse_json_array(sub_map []interface{}, indentation int) {
-  for val := range sub_map {
-  
-    //fmt.Print(get_space_string(indentation) + key + " => ")
-    
-    next_level_submap, result := sub_map[val].(map[string]interface{})
-    
-    if result != true {
-	    
-	    _, ok := sub_map[val].(string)	
-	    
-	    if !ok {
-	      _, ok := sub_map[val].([]interface{})
-	      
-	      if !ok {
-	        fmt.Println(sub_map[val].(float64)) 
-	      } else {
-	        recurse_json_array(sub_map[val].([]interface{}), indentation + 2)
-	     	  //fmt.Println(val.([]interface{}))
-	     	}
-	    } else {
-	      fmt.Println(sub_map[val].(string))
-	    }
 
-    } else {
-      fmt.Print("\n")
-      recurse_json(next_level_submap, indentation + 2)
-    }    
-  }
-}
-/////////////////////////////////////////////////////////////////////////////////
-func recurse_json(sub_map map[string]interface{}, indentation int) {
-  for key, val := range sub_map {
-  
-    fmt.Print(get_space_string(indentation) + key + " => ")
-    
-    next_level_submap, result := val.(map[string]interface{})
-    
-    if result != true {
-	    
-	    _, ok := val.(string)	
-	    
-	    if !ok {
-	      _, ok := val.([]interface{})
-	      
-	      if !ok {
-          _, ok := val.(float64)
-	      
-	        if !ok {
-	          fmt.Println(val.(bool))
-          } else{
-            fmt.Println(val.(float64))
-          }   
-	      } else {
-	        recurse_json_array(val.([]interface{}), indentation + 2)
-	     	  //fmt.Println(val.([]interface{}))
-	     	}
-	    } else {
-	      fmt.Println(val.(string))
-	    }
+func recurse_map(sub_map map[string]interface{}, indentation string, new_sub_map map[string]interface{}) {
 
-    } else {
-      fmt.Print("\n")
-      recurse_json(next_level_submap, indentation + 2)
-    }    
-  }
+	//println("[[[MAP]]]")
+
+	for index, elem := range sub_map {
+		//print("<" + reflect.TypeOf(elem).String() + ">")
+
+		new_sub_map[index] = elem
+
+		print(indentation + index + " => ")
+
+		if elem == nil {
+			continue;
+		}
+
+		interface_type := reflect.TypeOf(elem)
+
+		if interface_type.Kind() == reflect.Array || interface_type.Kind() == reflect.Slice {
+
+			array_length := len(elem.([]interface{}))
+
+			println()
+
+			new_sub_map[index] = make([]interface{}, array_length)
+			new_sub_map[index] = new_sub_map[index].([]interface{})
+
+			recurse_array(elem.([]interface{}), indentation + " ", new_sub_map[index].([]interface{}))
+
+		} else if interface_type.Kind() == reflect.Map {			
+			
+			println()
+
+			new_sub_map[index] = make(map[string]interface{})	
+
+			recurse_map(elem.(map[string]interface{}), indentation + " ", new_sub_map[index].(map[string]interface{}))
+
+		} else if interface_type.Kind() == reflect.String {
+
+			new_sub_map[index] = elem.(string)
+			println(indentation + elem.(string))
+
+		} else if interface_type.Kind() == reflect.Int {
+
+			println(indentation + string(elem.(int)))
+
+		} else if interface_type.Kind() == reflect.Float64 {
+
+			println(indentation + strconv.FormatFloat(elem.(float64), 'f', 2, 64))
+
+		}
+
+	}
 }
-/////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+func recurse_array(sub_map_array []interface{}, indentation string, new_sub_map_array []interface{}) {
+
+	//println("[[[ARRAY]]]")
+
+	for index, elem := range sub_map_array {
+		//print("<" + reflect.TypeOf(elem).String() + ">")
+
+		new_sub_map_array[index] = elem
+
+		print(indentation + strconv.Itoa(index) + " => ")
+
+		if elem == nil {
+			continue;
+		}
+		
+		interface_type := reflect.TypeOf(elem)
+
+		if interface_type.Kind() == reflect.Array || interface_type.Kind() == reflect.Slice {
+
+			array_length := len(elem.([]interface{}))
+
+			println()
+
+			new_sub_map_array[index] = make([]interface{}, array_length)
+			new_sub_map_array[index] = new_sub_map_array[index].([]interface{})
+
+			recurse_array(elem.([]interface{}), indentation + " ", new_sub_map_array[index].([]interface{}))
+
+		} else if interface_type.Kind() == reflect.Map {				
+				
+			new_sub_map_array[index] = make(map[string]interface{})	
+
+			recurse_map(elem.(map[string]interface{}), indentation + " ", new_sub_map_array[index].(map[string]interface{}))
+
+		} else if interface_type.Kind() == reflect.String {
+
+			new_sub_map_array[index] = elem.(string)
+			println(indentation + elem.(string))
+
+		} else if interface_type.Kind() == reflect.Int {
+
+			println(indentation + string(elem.(int)))
+
+		} else if interface_type.Kind() == reflect.Float64 {
+
+			println(indentation + strconv.FormatFloat(elem.(float64), 'f', 2, 64))
+
+		}
+
+	}
+}
+
+///////////////////////////////////////////////////////////
 
 func main() {
-   
-  filename := os.Args[1]
-  input_stream, _ := ioutil.ReadFile(filename)
+
+	filename := os.Args[1]
+
+	file_buffer, _ := ioutil.ReadFile(filename)
 	
-  top_level_map := make(map[string]interface{})
-	
-	json.Unmarshal([]byte(input_stream), &top_level_map)
-	
-	////////////////////////////////////////////////////////
-	
-	recurse_json(top_level_map, 0)  	
+	imported_map := make(map[string]interface{})
+	new_map := make(map[string]interface{})
+
+	json.Unmarshal([]byte(file_buffer), &imported_map)
+
+	recurse_map(imported_map, "", new_map)
+
+	println()
+
+	/////////////////
+
 }
 
